a1149518d386674dc865c42679801f0fbffaa4cd
HarshithBeta <144780325+HarshithBeta@users.noreply.github.com>
2025-02-19 20:30:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 4173, 
+    host: "0.0.0.0", 
+  },
   plugins: [react(),tailwindcss(),],
 })
