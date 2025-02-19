c87eb9b913a0c2cf37d07bb9209e80060fadf112
HarshithBeta <144780325+HarshithBeta@users.noreply.github.com>
2025-02-19 20:44:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: process.env.PORT || 4173, 
+    port: process.env.PORT || 5173, 
     host: "0.0.0.0", 
   },
-  allowedHosts: ["writeflow-eq7b.onrender.com"],
+  preview: {
+    port: process.env.PORT || 4173, 
+    host: "0.0.0.0",
+    allowedHosts: ["writeflow-eq7b.onrender.com"], 
+  },
   plugins: [react(),tailwindcss(),],
-})
+});
